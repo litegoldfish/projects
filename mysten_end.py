ee2b05a9b321c70b91440f94af407802e38713af
litegoldfish <tasha.ermolaeva@gmail.com>
2021-09-29 15:20:09 +0300

    Delete mysten_end.py

--- mysten_end.py
+++
@@ -1,6 +0,0 @@
-import os
-inp = "/Users/nata/Desktop/input"
-lst = os.listdir(inp)
-for fl in lst:
-    print(r'/Users/nata/Desktop/mystem ' + '/Users/nata/Desktop/input' + os.sep + fl + " /Users/nata/Desktop/out_texts" + os.sep + fl + ' -cnid —format xml' )
-    os.system(r'/Users/nata/Desktop/mystem ' + '/Users/nata/Desktop/input' + os.sep + fl + " /Users/nata/Desktop/out_texts" + os.sep + fl + ' -cnid —format xml' )
